ffbc0c6a84e989f14c5cd87f0fbc88e17b07f09a
Richar <ripupo88@gmail.com>
2020-09-27 20:23:34 +0100

    dfh

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
+import QrReader from 'react-qr-reader';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let result = 'sad';
+    const handleScan = (e) => {
+        console.log(e);
+    };
+    return (
+        <div>
+            <QrReader
+                delay={300}
+                onError={(e) => console.log(e)}
+                onScan={handleScan}
+                style={{ width: '100%' }}
+            />
+            <p>{result}</p>
+        </div>
+    );
 }
 
 export default App;
